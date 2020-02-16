45e5b1e1522a3eab9a5d0c0bb6fd127a924804f0
Christian Taylor <christianbtaylor@gmail.com>
2020-02-15 21:47:34 -0600

    Add mosquitto PHP extension

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ chsh -s $(which zsh)
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Install PHP extensions with PECL
-pecl install memcached imagick
+pecl install memcached imagick mosquitto-beta
 
 # Install global Composer packages
 /usr/local/bin/composer global require laravel/valet
