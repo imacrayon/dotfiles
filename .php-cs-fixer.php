747e5c07c12428d8758d3b96a8f592c4a4867696
Christian Taylor <christianbtaylor@gmail.com>
2022-06-13 15:54:05 -0500

    Update CS Fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,13 +1,19 @@
 <?php
 
-return PhpCsFixer\Config::create()
-    ->setRules([
+return (new PhpCsFixer\Config)->setRules([
         '@Symfony' => true,
         'array_syntax' => ['syntax' => 'short'],
         'increment_style' => ['style' => 'post'],
         'new_with_braces' => false,
         'no_empty_comment' => false,
-        'no_extra_blank_lines' => ['curly_brace_block', 'extra', 'throw', 'use'],
+        'no_extra_blank_lines' => [
+            'tokens' => [
+                'extra',
+                'throw',
+                'use',
+                'use_trait',
+            ],
+        ],
         'not_operator_with_successor_space' => true,
         'ordered_imports' => [
             'sort_algorithm' => 'alpha',
@@ -21,6 +27,6 @@ return PhpCsFixer\Config::create()
         'phpdoc_separation' => false,
         'php_unit_method_casing' => ['case' => 'snake_case'],
         'single_trait_insert_per_statement' => false,
-        'yoda_style' => null,
+        'yoda_style' => false,
     ])
     ->setLineEnding("\n");
